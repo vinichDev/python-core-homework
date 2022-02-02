15716e1cd147a8dc7c694ab9353bef192d559a06
Alexander Vinichenko <vinich.yota@gmail.com>
2022-02-02 14:08:12 +0300

    Completed Exercise 2

--- ex2/__init__.py
+++ ex2/__init__.py
@@ -1,3 +1,4 @@
+import time
 from ex2 import fetcher
 
 CALL_COUNT = 10
@@ -10,9 +11,19 @@ def benchmark(num):
     :param num: число итераций
     :return: функцию обёртку
     """
+
     def wrapper(func):
-        # put your code here
-        pass
+        def wrapped(*args, **kwargs):
+            sum_func_execution_time = 0
+            for i in range(num):
+                func_start_time = time.time()
+                func(*args, **kwargs)
+                func_end_time = time.time()
+                func_execution_time = func_end_time - func_start_time
+                sum_func_execution_time += func_execution_time
+                print(func_execution_time)
+            print(sum_func_execution_time / num)
+        return wrapped
     return wrapper
 
 
