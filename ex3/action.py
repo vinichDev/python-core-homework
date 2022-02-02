d140ecda1822e3219e389c8591f347ed3caba704
Alexander Vinichenko <vinich.yota@gmail.com>
2022-02-02 14:57:37 +0300

    Completed Exercise 3

--- ex3/action.py
+++ ex3/action.py
@@ -5,6 +5,12 @@ class BaseAction:
     def __repr__(self):
         return self.name
 
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
 
 class NothingAction(BaseAction):
     def __init__(self):
@@ -15,12 +21,30 @@ class RockAction(BaseAction):
     def __init__(self):
         super().__init__('Rock')
 
+    def __lt__(self, other):
+        return other.name == 'Paper'
+
+    def __gt__(self, other):
+        return other.name == 'Scissors'
+
 
 class PaperAction(BaseAction):
     def __init__(self):
         super().__init__('Paper')
 
+    def __lt__(self, other):
+        return other.name == 'Scissors'
+
+    def __gt__(self, other):
+        return other.name == 'Rock'
+
 
 class ScissorsAction(BaseAction):
     def __init__(self):
         super().__init__('Scissors')
+
+    def __lt__(self, other):
+        return other.name == 'Rock'
+
+    def __gt__(self, other):
+        return other.name == 'Paper'
