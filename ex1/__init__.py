0167c416dfd0939520ab842d042c23ee528de72e
Alexander Vinichenko <vinich.yota@gmail.com>
2022-02-02 12:20:34 +0300

    Completed Exercise 1

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -3,5 +3,21 @@ def build_roles_tree(mapping):
     :param mapping: маппинг ролей в категории
     :return: дерево ролей
     """
-    # put your code here
+    roles_tree = {"categories": []}
+    for category_id in mapping["categoryIdsSorted"]:
+        category_text = mapping["categories"][category_id]["name"]
+        category_items = []
+        for item_id in mapping["categories"][category_id]["roleIds"]:
+            item_text = mapping["roles"][item_id]["name"]
+            category_items.append({
+                "id": item_id,
+                "text": item_text
+            })
+        category = {
+            "id": f'category-{category_id}',
+            "text": category_text,
+            "items": category_items
+        }
+        roles_tree["categories"].append(category)
+    return roles_tree
     pass
